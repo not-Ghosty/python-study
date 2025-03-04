d3ee1cf6bb20ceb4f78bb6b0f966c797eebd3647
not-Ghosty <pradeep421999.pk@gmail.com>
2025-03-05 03:53:32 +0530

    learning string methods

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-print("Hello") 
+"""This is the beginning of python."""
+
+student_count = 200
+class_name = "        Python class"
+amount = 200.49
+
+print(len(class_name))
+print(class_name[0])
+print(class_name[0:9])
+print(class_name[:3])
+print(class_name[:])
+print(class_name.title())
+print(class_name.strip())
+print(class_name.upper())
+print(class_name.find("P"))
+print("py" in class_name)
+print("Py" in class_name)
+print("wy" not in class_name)
