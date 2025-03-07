6d42bac4d52e0a1736ed94a342ee894a25653923
not-Ghosty <pradeep421999.pk@gmail.com>
2025-03-07 12:59:13 +0530

    input functionality

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
 """This is the beginning of python."""
 
-student_count = 200
-class_name = "        Python class"
-amount = 200.49
+# student_count = 200
+# class_name = "        Python class"
+# amount = 200.49
 
-print(len(class_name))
-print(class_name[0])
-print(class_name[0:9])
-print(class_name[:3])
-print(class_name[:])
-print(class_name.title())
-print(class_name.strip())
-print(class_name.upper())
-print(class_name.find("P"))
-print("py" in class_name)
-print("Py" in class_name)
-print("wy" not in class_name)
+# print(len(class_name))
+# print(class_name[0])
+# print(class_name[0:9])
+# print(class_name[:3])
+# print(class_name[:])
+# print(class_name.title())
+# print(class_name.strip())
+# print(class_name.upper())
+# print(class_name.find("P"))
+# print("py" in class_name)
+# print("Py" in class_name)
+# print("wy" not in class_name)
+
+
+input_num = input("Enter any number here:")
+print(type(input_num))
+
+sum = int(input_num) + 2
+print(f" input: {input_num} , sum: {sum}")
+
+    
